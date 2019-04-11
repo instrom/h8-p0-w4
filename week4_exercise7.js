666a0d9cfd7fd4442142c95ab4f575cf8ed62dc1
Tommy Sutjipto <tommysutjipto96@gmail.com>
2019-04-11 10:56:31 +0700

    exercise7

--- week4_exercise7.js
+++ week4_exercise7.js
@@ -1,6 +1,24 @@
 function urutkanAbjad(str) {
-   var string = str.split('').sort().join('')
-    return string;
+    var string = '';
+    var arr = [];
+    for (var i=0; i < str.length; i++) {
+        arr.push(str[i])
+    }
+    var temp = 0;
+    for (var j=0; j < arr.length; j++) {
+      for (var k=0; k < arr.length - j - 1; k++) {
+        if(arr[k] > arr[k+1]) {
+          temp = arr[k];
+          arr[k] = arr[k+1];
+          arr[k+1] = temp
+        }
+      }
+    }
+    for (var l=0; l < arr.length; l++) {
+      string+= arr[l]
+    }
+    return string
+    
     // you can only write your code here!
   }
   
